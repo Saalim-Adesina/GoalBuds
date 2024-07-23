9e91e420bf4d2cd8b47e8a8536eacf4fda729636
Saalim Adesina <saalimayyubadesina@gmail.com>
2024-07-23 17:23:10 +0300

    Added basic layout and code

---
+++ Javascript/forms.js
@@ -0,0 +1,19 @@
+const {createServer} = require('https')
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let questions = ["What are you trying to improve in?", "Why do you want to improve in this goal?"]
+    let question = document.querySelector("#question")
+    let submit = document.querySelector("#form button")
+
+    submit.addEventListener("change", () => {
+
+    })
+
+
+    question.addEventListener("change", () => {
+
+    })
+
+    question.textContent
+})
